3eb6cf25221254fa3467806189561af494d6ac05
Maria Pankova <qitelopdo@gmail.com>
2026-03-17 20:22:28 +0300

    initiate index and app

---
+++ core/Application.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\core;
+
+use app\core\Routing\Router;
+
+class Application
+{   
+    public function __construct()
+    {
+        //
+    }
+
+
+    public function run()
+    {
+        //
+    }
+}
